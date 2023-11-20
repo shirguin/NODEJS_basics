8abc57e812d5bd6c6e8ba356aeb415d04cf27d75
shirguin <shirguin@yandex.ru>
2023-11-20 20:42:57 +0300

    Lesson_3

---
+++ Lesson_3/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Поступил запрос", req.method, req.url);
+  next();
+});
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  //res.send("<h1>Добро пожаловать на мой сайт!</h1>");
+  res.sendFile("static/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Страница обо мне</h1>");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на портк ${port}`);
+});
